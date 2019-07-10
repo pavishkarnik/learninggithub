7de7b8923487a3377d63651a7b9a381ad63a9ca1
Pavish_Karnik <pavish.karnik@marks-and-spencer.com>
2019-07-10 10:26:09 +0100

    initial commit

---
+++ src/main/java/com/learning/latesttech/jav8/JavSpApplication.java
@@ -0,0 +1,13 @@
+package com.learning.latesttech.jav8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavSpApplication.class, args);
+    }
+
+}
